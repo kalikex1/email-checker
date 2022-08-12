625f65822194a04706c8fc8eae02c0a58a76e722
Jonathon Tufts <86862338+jonathontufts@users.noreply.github.com>
2022-08-12 15:36:47 -0600

    style & function upgrades

--- test-react-app/src/components/FileUpload/index.js
+++ test-react-app/src/components/FileUpload/index.js
@@ -2,30 +2,60 @@ import React, { useState } from 'react';
 
 import './FileUpload.css'
 
+import fI from './fileIcon.png'
+import fDl from './dlFile.png'
+
 function FileUpload(props) {
     const [uploaded, setUploaded] = useState({})
+
+    const hiddenFileInput = React.useRef(null)
+    const hiddenFileDownload = React.useRef(null)
 
     function handleFilter(){
 
     }
 
+    function handleClick(e){
+        hiddenFileInput.current.click()
+    }
+
+    function handleChange(e){
+        console.log(e.target.files[0])
+        setUploaded({
+            file: e.target.files[0],
+            fName: e.target.files[0].name
+        })
+
+    }
+
+    function handleFilter(e){
+        console.log(uploaded.file, 'fuckyeah')
+    }
+
+    function handleDownload(){
+        hiddenFileDownload.current.click()
+    }
+
     return (
         <>
-        {/* <input type="file" accept='.csv' />
-        <p className='filterButton'>Filter Contacts</p>
+        <input type="file" accept='.csv' onChange={(e)=> handleChange(e)} ref={hiddenFileInput} style={{display:'none'}}/>
+        <a href={uploaded?.file} style={{display:'none'}} download='cleanFile' ref={hiddenFileDownload}></a>
         <div className='fileUploadWrap'>
 
-            <section className="container">
+            <div className="dirtyContainer" onClick={(e)=> handleClick(e)}>
 
-
-
-            </section>
-
-            <div className='cleanFileWrap'>
-
+                <img className='uploadIcon' src={fI}></img>
+                <p>{uploaded?.fName ? uploaded?.fName : 'Upload File'}</p>
             </div>
 
-        </div> */}
+            <p className='cleanButton' onClick={(e)=> handleFilter(e)}>Clean File</p>
+
+            <div className='cleanFileWrap'>
+                <img className='uploadIcon' src={fDl} onClick={(e)=> handleDownload(e)}></img>
+                <p>{uploaded?.fName ? uploaded?.fName : 'Download'}</p>
+            </div>
+
+        </div>
         </>
     );
 }
--- test-react-app/src/components/SplashPage/index.js
+++ test-react-app/src/components/SplashPage/index.js
@@ -11,7 +11,8 @@ function SplashPage() {
 
     const [resultClass, setResultClass] = useState('test')
 
-    async function singleSubmit(){
+    async function singleSubmit(e){
+        e.preventDefault()
         const result = await fetch(`https://aaront612.pythonanywhere.com/multi/${singleInput}`, { method: 'GET' })
             .then(response => response.json())
             .then(data => { return data })
@@ -26,40 +27,57 @@ function SplashPage() {
         console.log(singleResult)
         if (singleResult.Result === 'Invalid'){
             setResultClass('invalid')
-        }else{
+        } else if (singleResult.Result === 'Valid'){
             setResultClass('valid')
         }
     }, [singleResult])
     
 
-    if (!loaded) return (
-        <div className='loader'>
-            <h1>Loading...</h1>
-        </div>
-    )
+    // if (!loaded) return (
+    //     <div className='loader'>
+    //         <h1>Loading...</h1>
+    //     </div>
+    // )
 
 
     return (
+        // MAIN WRAP
         <div className='sHolder'>
-            {/* <div className='mainTop'> */}
+
+            {/* TOP */}
+
             <div className='singleBox'>
-                <div>
-                    <label className="custom-field one">
-                        <input type="email" placeholder=" " onChange={(e) => { setSingleInput(e.target.value) }} />
-                        <span className="placeholder">Email Address</span>
-                    </label>
-
-                    <div onClick={singleSubmit}>Verify</div>
+                <div className='emailBox'>
+                        <div className='eInput'>
+                            <form onSubmit={(e) => singleSubmit(e)}>
+                                <input type="email" placeholder="Email Address" onChange={(e) => { setSingleInput(e.target.value) }} />
+                                {/* <div onClick={singleSubmit}>Verify</div> */}
+                                <button type='submit'>Verify</button>
+                            </form>
+                        </div>
+                        <div className='eResults'>
+                        <p className={resultClass}>{singleResult?.Result ? singleResult?.Input + ' : ' + singleResult?.Result : 'Email - Test For Results'}</p>
+                        <p>Reason: {singleResult?.Reason ? singleResult?.Reason : 'N/A'}</p>
+                        </div>
                 </div>
-                    <h4 className={resultClass}>{singleResult?.Input}: {singleResult?.Result}</h4>
-                    <p>Reason: {singleResult?.Reason}</p>
+                <div className='phoneBox'>
+                    <div className='pInput'>
+                        <form onSubmit={(e) => singleSubmit(e)}>
+                            <input type="tel" placeholder="Phone Number" onChange={(e) => { setSingleInput(e.target.value) }} />
+                            {/* <div onClick={()=> singleSubmit(e)}>Verify</div> */}
+                            <button type='submit'>Verify</button>
+                        </form>
+                    </div>
+                    <div className='pResults'>
+                        <p className={resultClass}>{singleResult?.Result ? singleResult?.Input + ' : ' + singleResult?.Result : 'Phone - Test For Results'}</p>
+                        <p>Phone Carrier: {singleResult?.Reason ? singleResult?.Reason : 'N/A'}</p>
+                    </div>
+                </div>
             </div>
-            {/* </div> */}
 
-            {/*  */}
+            {/* BOTTOM */}
 
             <div className='mainBottom'>
-                <h4>File Upload</h4>
                 <FileUpload />
             </div>
 
