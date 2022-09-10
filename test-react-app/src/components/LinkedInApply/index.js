fff0dc6cf4a4b120a53b6a5938145f8751074bb1
Jonathon Tufts <86862338+jonathontufts@users.noreply.github.com>
2022-09-10 10:15:31 -0600

    more touches

--- test-react-app/src/components/LinkedInApply/index.js
+++ test-react-app/src/components/LinkedInApply/index.js
@@ -4,14 +4,27 @@ import './linkedIn.css'
 function LinkedInApply() {
 
 
+    const [eName, setEName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+
 
     return (
         <div className='learnMoreWrap'>
             <div className='subBox'>
+                <label> Subscribe for...</label>
+                <p>- Lifetime Deal on Contact Hero+</p>
+                <p>- Stay up to date with Releases</p>
+                <p>- </p>
 
             </div>
             <div className='contactForm'>
-
+                <label>Questions & Feedback Form</label>
+                <input type='text' placeholder='Name'></input>
+                <input type='email' placeholder='Email'></input>
+                <textarea placeholder='Type Here ...'></textarea>
+                <button>Submit</button>
             </div>
         </div>
     );
