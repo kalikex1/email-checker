7f9e88043fcd38d1bfd545927e8537c41c2e76bd
Jonathon Tufts <86862338+jonathontufts@users.noreply.github.com>
2022-09-29 23:34:13 -0600

    feedback / subscribe form

--- test-react-app/src/components/ContactPage/index.js
+++ test-react-app/src/components/ContactPage/index.js
@@ -1,5 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
 import './ContactPage.css'
+
+
+
+
+import fb from '../../images/fb.png'
+import tw from '../../images/twitter.png'
+import linkedin from '../../images/linkedin.png'
+import emailPic from '../../images/email.png'
 
 function ContactPage() {
 
@@ -11,29 +21,45 @@ function ContactPage() {
     const [subEmail, setSubEmail] = useState('')
 
     const [showMessage, setShowMessage] = useState(false)
+    const [showFeedback, setShowFeedback] = useState(false)
 
     useEffect(() => {
         let didSub = localStorage.getItem('didSub')
 
         if (didSub){
             setShowMessage(true)
-            console.log(didSub)
         }
 
         console.log('hi')
 
     }, []);
 
+    const form1 = useRef()
+
     function newSub(){
-        console.log(subEmail)
+        emailjs.sendForm('service_qhrxk5e', 'template_fr4x3zz', form1.current, 'GGmPRk_hda6Hc9Cwd')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
         localStorage.setItem('didSub', true)
         setShowMessage(true)
-        console.log(localStorage.getItem('didSub'))
     }
 
+    const form2 = useRef()
 
     function newMessage() {
-
+        emailjs.sendForm('service_qhrxk5e', 'template_rd3x4zp', form2.current, 'GGmPRk_hda6Hc9Cwd')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        setEName('')
+        setEmail('')
+        setMessage('')
+        setShowFeedback(true)
     }
 
 
@@ -42,36 +68,50 @@ function ContactPage() {
         <div className='learnMoreWrap'>
             {showMessage && (
                 <>
-                <h1>
-                    no
-                </h1>
+                <div className='subbedBox'>
+                    <h2>
+                        Thank you for subscribing! 
+                    </h2>
+                    <p onClick={()=> {setShowMessage(false); localStorage.removeItem('didSub')}}>add another email?</p>
+                    <div className="navRight">
+                        <h5>Share: </h5>
+                        <div className="shareHolder">
+                            <img onClick={(e) => { window.open("https://contacthero.tech", '_blank') }} src={fb} alt="fb" />
+                            <img onClick={(e) => { window.open("https://contacthero.tech", '_blank') }} src={tw} alt="tw" />
+                            <img onClick={(e) => { window.open("https://contacthero.tech", '_blank') }} src={linkedin} alt="linkedin" />
+                            <img onClick={(e) => { window.open("https://contacthero.tech", '_blank') }} src={emailPic} alt="email" />
+                        </div>
+                    </div>
+                </div>
                 </>
             )}
             {!showMessage && (
-                <h1>
-                    hi
-                </h1>
+                <>
+                    <div className='subBox'>
+                        <div className='topInfo'>
+                            <h3> Subscribe for...</h3>
+                            <p>- Lifetime Deal on Contact Hero +</p>
+                            <p>- Stay up to date with Releases</p>
+                        </div>
+                        <div className='email-Input'>
+                            <form ref={form1} onSubmit={(e)=> {e.preventDefault(); newSub()}}>
+                                <input required name='user_email' type='email' onChange={(e)=> {setSubEmail(e.target.value)}}></input>
+                                <button type='submit'>Subscribe</button>
+                            </form>
+                        </div>
+                    </div>
+                </>
             )}
-            <div className='subBox'>
-                <div className='topInfo'>
-                    <h3> Subscribe for...</h3>
-                    <p>- Lifetime Deal on Contact Hero +</p>
-                    <p>- Stay up to date with Releases</p>
-                </div>
-                <div className='email-Input'>
-                    <form onSubmit={(e)=> {e.preventDefault(); newSub()}}>
-                        <input type='email' onChange={(e)=> {setSubEmail(e.target.value)}}></input>
-                        <button type='submit'>Subscribe</button>
-                    </form>
-                </div>
 
-            </div>
             <div className='contactForm'>
-                <label>Questions & Feedback Form</label>
-                <input type='text' placeholder='Name'></input>
-                <input type='email' placeholder='Email'></input>
-                <textarea placeholder='Type Here ...'></textarea>
-                <button>Submit</button>
+                <h3>Questions & Feedback</h3>
+                {showFeedback && ( <p>Message Received !</p> )}
+                <form ref={form2} onSubmit={(e)=> {e.preventDefault(); newMessage()}}>
+                    <input name='user_name' value={eName} required type='text' placeholder='Name' onChange={(e) => { setEName(e.target.value) }}></input>
+                    <input name='user_email' value={email} required type='email' placeholder='Email' onChange={(e)=> {setEmail(e.target.value)}}></input>
+                    <textarea name='message' value={message} required placeholder='Type Here ...' onChange={(e) => { setMessage(e.target.value) }}></textarea>
+                    <button>Submit</button>
+                </form>
             </div>
         </div>
     );
